52226972d4c8b9dc468730ce0a349525a2175d6d
Tanghj <3279179450@qq.com>
2025-05-11 18:17:44 +0800

    projects

---
+++ Different Layouts/app/src/main/java/com/example/android_practice/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.android_practice;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
